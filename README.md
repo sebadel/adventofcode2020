0b1c0652826cf9e533fb2405e17396541cbb767b
Sébastien Delneste <seb@delneste.be>
2020-12-03 10:06:27 +0100

    Day 3

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ I made one mistake considering the valdity of a password.
 I only considered valid a password if the first check was True and the second one was False.
 This actually was validated by my test suite.
 I ended up with a wrong answer until I considered that another valid password could be mismatch on the first char and a match on the 2nd char.
+
+## Day 03: Toboggan slope trajectory
+### Part 1
+ - Wasted time because I failed to strip the input data :(
+
+### Part 2
+ - Learned math.prod is only available in Python >= 3.8
+ - Forgot to start parsing the input from line == down_step. data[down_step:0] instead of data[1:]
+
---
+++ day03/day03.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+
+def part1(data):
+    x = 0
+    cnt = 0
+    for line in data[1:]:
+        x += 3
+        x = x % len(line)
+        if line[x] == '#':
+            cnt += 1
+    return cnt                
+
+def part2(data):
+    simulations = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ]
+    total = 1
+    for s in simulations:
+        total = total * _part2(data, s[0], s[1])
+    return total
+
+
+def _part2(data, right_step, down_step):
+    x = 0
+    cnt = 0
+    for line_number, line in enumerate(data[down_step:]):
+        if line_number % down_step != 0:
+            continue
+        x += right_step
+        x = x % len(line)
+        if line[x] == '#':
+            cnt += 1
+    return cnt
+
+
+def main():
+    data = [l.strip() for l in open('input.txt', 'r').readlines()]
+    print('Part 1: %d' % part1(data))
+    print('Part 2: %d' % part2(data))
+
+
+if __name__ == "__main__":
+    main()
